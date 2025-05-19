b01071e238d796d9f25113c77f9b7eb3f209e1b1
Gabriel Nunes <gabrielnunes.service@gmail.com>
2025-05-19 16:11:22 -0300

    Projeto inicial: backend e frontend

---
+++ vehiclerental-api/vehiclerental-api/src/main/java/com/jonatas/vehiclerental/vehiclerental_api/VehiclerentalApiApplication.java
@@ -0,0 +1,13 @@
+package com.jonatas.vehiclerental.vehiclerental_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiclerentalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiclerentalApiApplication.class, args);
+	}
+
+}
---
+++ vehiclerental-api/vehiclerental-api/src/test/java/com/jonatas/vehiclerental/vehiclerental_api/VehiclerentalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonatas.vehiclerental.vehiclerental_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiclerentalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
